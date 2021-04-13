b8ede1b401a0bbfe1e5b1e3e5635e4bde9fe66de
Mo Morf <J-a-c-k@yandex.ru>
2021-04-13 21:38:45 +0500

    Добавьте файлы проекта.

---
+++ FrequencyAnalysisTask.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace TextAnalysis
+{
+    static class FrequencyAnalysisTask
+    {
+        public static Dictionary<string, string> GetMostFrequentNextWords(List<List<string>> text)
+        {
+            var result = new Dictionary<string, string>();
+            var wordsCount = new Dictionary<string, int>();
+            var sentences = text;
+
+            //string.CompareOrdinal();
+            //result.Comparer;
+
+            foreach (var item in sentences)
+            {
+                foreach (var words in item)
+                {
+                    if (!wordsCount.ContainsKey(words)) wordsCount[words] = 1;
+                    else wordsCount[words]++;
+                }
+            }
+
+            for (int i = 0; i < sentences.Capacity; i++)
+            {
+                for (int j = 0; j < sentences[i][j].Length; j++)
+                {
+                    if (sentences[0].Capacity > 2)
+                    {
+                        if (!result.ContainsKey(sentences[i][j])) result[sentences[i][j]] = sentences[i][j+1];
+                    }
+                }
+            }
+
+            return result;
+        }
+   }
+}
---
+++ SentencesParserTask.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace TextAnalysis
+{
+    static class SentencesParserTask
+    {
+        static void RemoveControl(string[] str)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                str[i] = str[i].ToLower();
+                str[i] = str[i].Replace('\t', ' ');
+                str[i] = str[i].Replace('\r', ' ');
+                str[i] = str[i].Replace('\n', ' ');
+            }
+        }
+
+        static List<List<string>> FormatingProcess(string[] str, List<List<string>> sentencesList)
+        {
+            var builder = new StringBuilder();
+            var wordsList = new List<string>();
+            string word = null;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                for (int j = 0; j < str[i].Length; j++)
+                {
+                    if (char.IsLetter(str[i][j]) || str[i][j] == '\'')
+                    {
+                        builder.Append(str[i][j]);
+                        if (j == str[i].Length - 1)
+                        {
+                            word = builder.ToString();
+                            if (word != "") wordsList.Add(word);
+                        }
+                    }
+                    else
+                    {
+                        word = builder.ToString();
+                        if (word != "") wordsList.Add(word);
+                        builder.Clear();
+                    }
+                }
+                if (wordsList.Count != 0) sentencesList.Add(wordsList.ToList());
+                wordsList.Clear();
+                builder.Clear();
+            }
+
+            return sentencesList;
+        }
+
+        public static List<List<string>> ParseSentences(string text)
+        {
+            var sentencesList = new List<List<string>>();
+            char[] split = new char[] { '.', '!', '?', ';', ':', '(', ')' };
+            string[] str = text.Split(split);
+
+            RemoveControl(str);
+
+            return FormatingProcess(str, sentencesList);
+        }
+    }
+}
